c4ea4c7c6b5f376b468b49c08b8eafc63a278989
spectrevuln <54567102+Spectrevuln-sketch@users.noreply.github.com>
2020-05-09 14:42:03 -0400

    Update homeItems.js

--- src/vars/homeItems.js
+++ src/vars/homeItems.js
@@ -2,7 +2,7 @@ var myName = 'Gerry Raditya Kusmana Yahya'
 var myOccupation = 'Ethical Hacking, IT QA'
 var myDetails = 'My name is Gerry Raditya Kusmana Yahya Before I worked at PT Gs Retail Indonesia as an IT QA, I also participated in an ethical hacking platform, on that platform I learned about hacking procedures and get to know more about what programs the web uses and updates about security levels the web'
 var myImageLink = "https://i.imgur.com/N5N2cFC.jpg"
-var myGithub = 'https://github.com/SpectreVuln'
+var myGithub = 'https://github.com/Spectrevuln-sketch'
 
 module.exports = {
     myName, myOccupation, myDetails, myImageLink, myGithub
